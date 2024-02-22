8c7de1c061701c2006c03fc78c31e8a9dd6e7ab4
taro710 <katayamayutaro@team-lab.com>
2024-02-22 18:05:09 +0900

    default files

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Custom model collection
+
+Collection of custom models created mainly in blender
+
+## Setup
+
+```bash
+npm install
+
+npm run dev
+
+npm run build
+```
---
+++ src/script.js
@@ -0,0 +1,172 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import GUI from "lil-gui";
+
+// URL
+const url = new URL(window.location.href);
+let customModelPath;
+switch (url.pathname) {
+  case "/":
+    customModelPath = "/models/capsule.glb";
+    break;
+  case "/hamburger":
+    customModelPath = "/models/hamburger.glb";
+    break;
+  //   default:
+  //     break;
+}
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Environment map
+ */
+scene.backgroundBlurriness = 0;
+scene.backgroundIntensity = 1;
+const textureLoader = new THREE.TextureLoader();
+
+const environmentMap = textureLoader.load(
+  "/environmentMaps/neon_city_night.jpg"
+);
+environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+environmentMap.colorSpace = THREE.SRGBColorSpace;
+
+scene.background = environmentMap;
+
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, {
+  type: THREE.FloatType,
+});
+
+scene.environment = cubeRenderTarget.texture;
+// Cube camera
+const cubeCamera = new THREE.CubeCamera(0.1, 100, cubeRenderTarget);
+cubeCamera.layers.set(1);
+
+/**
+ * Models
+ */
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/draco/");
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+
+let mixer = null;
+
+gltfLoader.load(customModelPath, (gltf) => {
+  const model = gltf.scene;
+  model.position.y = 0;
+
+  scene.add(model);
+
+  scene.add(gltf.scene);
+});
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+  new THREE.CylinderGeometry(5, 5, 0.2, 50),
+  new THREE.MeshStandardMaterial({
+    color: "#444444",
+    metalness: 0,
+    roughness: 0.2,
+  })
+);
+floor.receiveShadow = true;
+scene.add(floor);
+
+// Ring light
+const holyDonut = new THREE.Mesh(
+  new THREE.TorusGeometry(15, 0.5),
+  new THREE.MeshBasicMaterial({ color: new THREE.Color(10, 4, 2) })
+);
+holyDonut.layers.enable(1);
+holyDonut.position.y = 1;
+scene.add(holyDonut);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(-8, 4, 8);
+scene.add(camera);
+
+/**
+ * Controls
+ */
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 1, 0);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animation
+ */
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+
+  if (holyDonut) holyDonut.rotation.x = Math.sin(elapsedTime) * 2;
+  cubeCamera.update(renderer, scene);
+
+  if (mixer) mixer.update(deltaTime);
+
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
